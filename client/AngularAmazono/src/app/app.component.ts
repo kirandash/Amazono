887bcb5f2286392b1cce25a581f5ec8cb3c5436c
kirandash <thekirankumardash@gmail.com>
2019-01-29 23:57:10 +0530

    11 - Setting up Navbar and Footer

--- client/AngularAmazono/src/app/app.component.ts
+++ client/AngularAmazono/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularAmazono';
+  searchTerm = '';
+  isCollapsed = true; // To check if mobile menu is expanded or collapsed for mobile view of app
+
+  get token() {
+    return localStorage.getItem('token'); // used for authentication feature
+  }
+
+  collapse() {
+    this.isCollapsed = true; // used to make mobile menu collapse
+  }
+
+  closeDropdown(dropdown) {
+    dropdown.close(); // used to close navbar dropdown
+  }
+
+  logout() { } // used for logout function
+
+  search() { } // used for search function
 }
